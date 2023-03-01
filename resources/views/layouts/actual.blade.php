3e7dc2181a9b25e36d5e140fc4b2343e18cc30ff
astorgazx <64606815+astorgazx@users.noreply.github.com>
2023-03-01 11:36:43 +0000

    Se agrego el layout actual.blade.php para que se pueda usar en las vistas de productos y en las de categorias

---
+++ resources/views/layouts/actual.blade.php
@@ -0,0 +1,66 @@
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+    <title>Residencia</title>
+
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+    <nav class="navbar navbar-dark navbar-expand-md" style="background: #851e39;">
+        <div class="container-fluid"><a class="navbar-brand" href="#" style="color: rgb(239,228,156);">Distribuidora ANDASO</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div id="navcol-1" class="collapse navbar-collapse">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link active" href="#">Nuestros Productos</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="#">! Forma parte de nuestra comunidad !</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="#">Eres Socio?</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="#">Cerrar Seccion</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <section style="background: #c99d84;">
+    <div class="container">
+        @yield('content')
+
+    </div>
+
+
+
+
+    </section>
+    <footer class="footer-dark" style="background: #851e39;">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-md-3 text-center align-self-center item">
+                    <h3 style="color: rgb(239,228,156);">Contactanos</h3>
+                </div>
+                <div class="col-sm-6 col-md-3 item">
+                    <h3 style="color: rgb(239,228,156);">Sobre Nosotros</h3>
+                    <ul>
+                        <li><a href="#">Organizacion</a></li>
+                        <li><a href="#">Proximos Eventos</a></li>
+                        <li><a href="#">Noticias</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 item text">
+                    <h3 style="color: rgb(239,228,156);">Encuentranos En :</h3>
+                    <ul>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Instragram</a></li>
+                    </ul>
+                </div>
+            </div>
+            <p class="copyright">ANDASO © 2023</p>
+        </div>
+    </footer>
+</body>
+
+</html>
--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -1,32 +1,51 @@
-<html>
-<head>
-    <title>Productos</title>    
-</head>
-<body>
-    <h1>Productos</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>Nombre</th>
-                <th>Descripción</th>
-                <th>Precio</th>
-                <th>Stock</th>
-                <th>Categoría</th>
-                <th>Imagen</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($productos as $producto)
-            <tr>
-                <td>{{ $producto->nombre }}</td>
-                <td>{{ $producto->descripcion }}</td>
-                <td>{{ $producto->precio }}</td>
-                <td>{{ $producto->stock }}</td>
-                <td>{{ $producto->categoria_id }}</td>
-                <td>{{ $producto->imagen }}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-</body>
-</html>
+@extends('layouts.actual')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>Productos</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <a href="#" class="btn btn-success">Crear</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Descripción</th>
+                        <th>Precio</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($productos as $producto)
+                        <tr>
+                            <td>{{ $producto->id }}</td>
+                            <td>{{ $producto->nombre }}</td>
+                            <td>{{ $producto->descripcion }}</td>
+                            <td>{{ $producto->precio }}</td>
+                            <td>
+                                <a href="{{ route('productos.show', $producto->id) }}" class="btn btn-primary">Ver</a>
+                                <a href="{{ route('productos.edit', $producto->id) }}" class="btn btn-warning">Editar</a>
+                                <form action="{{ route('productos.destroy', $producto->id) }}" method="POST" style="display: inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">Eliminar</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+@endsection

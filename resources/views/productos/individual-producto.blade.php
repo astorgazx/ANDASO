e9838c0590bb506061a26022d616881b8a9c43e1
astorgazx <64606815+astorgazx@users.noreply.github.com>
2023-03-23 05:23:15 +0000

    Se actualizo el archivo composer.lock y se agrego el archivo .gitignore para que no se suban los archivos de composer

--- resources/views/productos/individual-producto.blade.php
+++ resources/views/productos/individual-producto.blade.php
@@ -14,6 +14,31 @@
     </div>
 </div>
 
+<!-- Modal para realizar la compra -->
+
+<div class="modal" id="compraModal" aria-hidden="true" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Compra</h5>
+                <!-- Metodos de pago -->
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Hacer el script para el modal -->
+
+<script>
+    $(document).ready(function() {
+        $('#compraModal').modal('show');
+    });
+</script>
+
+
+
 @endsection
 
 

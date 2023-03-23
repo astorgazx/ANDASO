6d1008bcf5237ba3450541d32c1fe98dd213bd08
astorgazx <64606815+astorgazx@users.noreply.github.com>
2023-03-23 06:01:52 +0000

    Se modifico la vista de clientes

--- resources/views/admin/clientes/index.blade.php
+++ resources/views/admin/clientes/index.blade.php
@@ -1,15 +1,42 @@
-<html>
-    <head>
-        <title>Clientes</title>
-    </head>
-    <body>
-        <h1>Clientes</h1>
-        <ul>
-            @foreach ($clientes as $cliente)
-                <li>{{ $cliente->nome }}</li>
-            @endforeach
-        </ul>
-    </body>
+
+@extends('layouts.actual')
+
+@section('content')
+
+<!-- Crear una tabla para mostra la informacion de los clientes -->
 
 
-</html>
+<div class="row">
+    <div class="col-md-auto">
+        <h3 class="text-center">Clientes</h3>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Apellido</th>
+                    <th scope="col">Correo</th>
+                    <th scope="col">Telefono</th>
+                    <th scope="col">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($clientes as $cliente)
+                <tr>
+                    <td>{{ $cliente->nombreCliente }}</td>
+                    <td>{{ $cliente->apPaternoCliente }}</td>
+                    <td>{{ $cliente->emailCliente }}</td>
+                    <td>{{ $cliente->telefonoCliente }}</td>
+                    <td>
+                        <a href="{{ route('admin.clientes.show', $cliente->id) }}" class="btn btn-primary">Ver</a>
+                        <a href="{{ route('admin.clientes.edit', $cliente->id) }}" class="btn btn-warning">Editar</a>
+                        <form action="{{ route('admin.clientes.destroy', $cliente->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Eliminar</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
